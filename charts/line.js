9e4cf3f1f00e3b6179f14fa41f23e0a0b12e4e05
Roi Avinoam <avinoamr@gmail.com>
2015-11-09 02:28:48 +0200

    Beginning of the line chart

--- charts/line.js
+++ charts/line.js
@@ -16,6 +16,8 @@
         el.innerHTML = "<svg></svg>";
     }
 
+    Line.prototype._palette = window.color.palettes.default;;
+
     Line.prototype.data = autodraw( getset( "_data" ) );
     Line.prototype.x = autodraw( getset( "_x" ) );
     Line.prototype.y = autodraw( getset( "_y" ) );
@@ -45,7 +47,107 @@
             .style( "height", "100%" )
             .style( "width", "100%" );;
 
-        
+        var _x = that._x, _c = that._color, _y = that._y;
+
+        // stack by colors
+        var points = that._data
+        var extent = d3.extent( points, function ( d ) { return d[ _x ] } );
+        var data = d3.nest()
+            .key( function ( d ) { return d[ _c ]  || "" } )
+            .rollup( function ( data ) {
+
+                // if this line begins after the chart's start, add a zero-
+                // height segment to precede it
+                var minx = d3.min( data, function ( d ) { return d[ _x ] } );
+                if ( minx != extent[ 0 ] ) {
+                    var min1 = {}; min1[ _y ] = 0; min1[ _x ] = minx;
+                    var min2 = {}; min2[ _y ] = 0; min2[ _x ] = extent[ 0 ];
+                    data.unshift( min1, min2 );
+                }
+
+                // if this line begins before the chart's end, add a zero-
+                // height segment to succeed it
+                var maxx = d3.max( data, function ( d ) { return d[ _x ] } );
+                if ( maxx != extent[ 1 ] ) {
+                    var max1 = {}; max1[ _y ] = 0; max1[ _x ] = maxx;
+                    var max2 = {}; max2[ _y ] = 0; max2[ _x ] = extent[ 1 ];
+                    data.push( max1, max2 )
+                }
+
+                // sort it by the x-coordinate to make sure the path the drawing
+                // is sane
+                data.sort( function ( d1, d2 ) {
+                    return d1[ _x ] - d2[ _x ];
+                })
+
+                return data;
+            })
+            .entries( that._data );
+
+        var x = d3.scale.linear()
+            .domain( d3.extent( points, function ( d ) { return d[ _x ] } ) )
+            .range( [ 0, svg.node().offsetWidth ] )
+
+        var y = d3.scale.linear()
+            .domain( [ 0, d3.max( points, function ( d ) { return d[ _y ] } ) ] )
+            .range( [ 0, svg.node().offsetHeight ] );
+
+        var allc = data.map( function ( d ) { return d.key } );
+        var clin = d3.scale.linear()
+            .domain( d3.extent( allc ) )
+            .range( [ that._palette.from, that._palette.to ] );
+
+        var cord = d3.scale.ordinal()
+            .domain( allc )
+            .range( that._palette )
+
+        var c = that._palette.from && that._palette.to ? clin : cord;
+
+        var area = d3.svg.area()
+            .x( function ( d ) { return x( d[ _x ] ) } )
+            .y0( y.range()[ 1 ] )
+            .y1( function ( d ) { return y.range()[ 1 ] - y( d[ _y ] ) } )
+
+        var line = d3.svg.line()
+            // .interpolate( "cardinal" )
+            .tension( .9 )
+            .x( function ( d ) { return x( d[ _x ] ) } )
+            .y( function ( d ) { return y.range()[ 1 ] - y( d[ _y ] ) } )
+
+        var lines = svg.selectAll( "path[data-line]" )
+            .data( data )
+
+        lines.exit().remove();
+        lines.enter().append( "path" )
+            .attr( "data-line", function ( d ) {
+                return d.key;
+            })
+            .attr( "d", function ( d ) { 
+                return line( d.values ) 
+            })
+            .attr( "fill", "none" )
+            .attr( "stroke", function ( d ) {
+                return c( d.key );
+            })
+            .attr( "stroke-width", 3 )
+            .style( "opacity", 1 )
+
+        var areas = svg.selectAll( "path[data-area]" )
+            .data( data );
+
+        areas.exit().remove()
+        areas.enter().append( "path" )
+            .attr( "data-area", function ( d ) {
+                return d.key;
+            })
+            .attr( "d", function ( d ) { 
+                return area( d.values ) 
+            })
+            .attr( "fill", function ( d ) {
+                return c( d.key );
+            })
+            .attr( "stroke", "none" )
+            .style( "opacity", .1 )
     }
 
 
