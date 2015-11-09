49b891556f7f746f29930713901a50ddf74da8a8
Roi Avinoam <avinoamr@gmail.com>
2015-11-09 18:14:59 +0200

    Support stacking, although the code isn't perfect yet

--- charts/line.js
+++ charts/line.js
@@ -53,13 +53,12 @@
 
         // extract the values for each obj
         var data = that._data.map( function ( d ) {
-            return { x: d[ _x ], y: d[ _y ], c: d[ _c ], obj: d }
+            return { x: d[ _x ], y: d[ _y ], y0: 0, c: d[ _c ], obj: d }
         })
         var isTimeline = ( data[ 0 ] || {} ).x instanceof Date;
 
-        // stack by colors
-        var xExtent = d3.extent( data, function ( d ) { return d.x });
-        var yExtent = d3.extent( data, function ( d ) { return d.y });
+        // group by colors
+        var xExtent = d3.extent( data, function ( d ) { return d.x } );
         data = d3.nest()
             .key( function ( d ) { return d.c  || "" } )
             .rollup( function ( data ) {
@@ -67,14 +66,20 @@
                 // height segment to precede it
                 var minx = d3.min( data, function ( d ) { return d.x } );
                 if ( minx != xExtent[ 0 ] ) {
-                    data.unshift( { x: minx, y: 0 }, { x: xExtent[ 0 ], y :0 });
+                    data.unshift( 
+                        { x: minx - .01, y: 0, y0: 0 }, 
+                        { x: xExtent[ 0 ], y: 0, y0: 0 }
+                    );
                 }
 
                 // if this line begins before the chart's end, add a zero-
                 // height segment to succeed it
                 var maxx = d3.max( data, function ( d ) { return d.x } );
                 if ( maxx != xExtent[ 1 ] ) {
-                    data.push( { x: maxx, y: 0 }, { x: xExtent[ 1 ], y: 0 } )
+                    data.push( 
+                        { x: maxx + .01, y: 0, y0: 0 }, 
+                        { x: xExtent[ 1 ], y: 0, y0: 0 }
+                    )
                 }
 
                 // sort it by the x-coordinate to make sure the path the drawing
@@ -87,11 +92,50 @@
             })
             .entries( data );
 
+        // fill-in the gaps
+        var leaves = color.tree.dfs()
+            .filter( function ( d, i, j ) { return j == 2 } )
+            .values( function ( d ) { return d.values || d } )
+            .entries( data );
+
+        var xAll = leaves.map( function ( d ) { return d.x });
+
+        data.map( function ( d ) {
+            return d.values;
+        })
+        .forEach( function ( data ) {
+            xAll.forEach( function ( x ) {
+                var closest = data.reduce( function ( closest, d, i ) {
+                    return d.x > x ? closest : { x: d.x, y: d.y, i: i };
+                }, { x: data[ 0 ].x, y: data[ 0 ].y, i: 0 } );
+
+                if ( x == closest.x ) return; 
+                var after = data[ closest.i + 1 ];
+
+                var total = after.x - closest.x;
+                var part = x - closest.x;
+
+                var y = d3.interpolate( closest.y, after.y )( part / total );
+                data.splice( closest.i + 1, 0, { x: x, y: y, y0: 0 } )
+            })
+        })
+
+        console.log( data );
+
+        // stacked graph
+        var stack = d3.layout.stack()
+            .values( function ( d ) { return d.values })
+
+        if ( that._stack ) {
+            stack( data );
+        }
+
         // build the scales
         var x = ( isTimeline ? d3.time.scale() : d3.scale.linear() )
             .domain( xExtent )
             .range( [ 0, svg.node().offsetWidth ] )
 
+        var yExtent = d3.extent( leaves, function ( d ) { return d.y + d.y0 } );
         var y = d3.scale.linear()
             .domain( [ 0, yExtent[ 1 ] ] )
             .range( [ 4, svg.node().offsetHeight ] );
@@ -109,15 +153,17 @@
 
         var area = d3.svg.area()
             .x( function ( d ) { return x( d.x ) } )
-            .y0( y.range()[ 1 ] )
+            .y0( function ( d ) {
+                return y.range()[ 1 ] + y.range()[ 0 ] - y( d.y0 );
+            })
             .y1( function ( d ) { 
-                return y.range()[ 1 ] - y( d.y ) + y.range()[ 0 ];
+                return y.range()[ 1 ] - y( d.y0 + d.y ) + y.range()[ 0 ];
             })
 
         var line = d3.svg.line()
             .x( function ( d ) { return x( d.x ) } )
             .y( function ( d ) { 
-                return y.range()[ 1 ] - y( d.y ) + y.range()[ 0 ] 
+                return y.range()[ 1 ] - y( d.y0 + d.y ) + y.range()[ 0 ] 
             })
 
         // start drawing
@@ -165,21 +211,23 @@
             .attr( "fill", function ( d ) {
                 return c( d.key );
             })
-            .style( "opacity", that._stack ? 1 : .1 );
+            .style( "opacity", that._stack ? .4 : .1 );
 
         var points = groups.selectAll( "circle[data-point]" )
             .data( function ( d ) { return d.values })
         points.exit().remove()
         points.enter().append( "circle" )
             .attr( "data-point", "" )
-            .attr( "r", 2 )
+            .attr( "r", function () {
+                return 2;
+            })
 
         points
             .attr( "cx", function ( d ) { 
                 return x( d.x ) 
             })
             .attr( "cy", function ( d ) {
-                return y.range()[ 1 ] - y( d.y ) + y.range()[ 0 ]
+                return y.range()[ 1 ] - y( d.y0 + d.y ) + y.range()[ 0 ]
             })
             .attr( "fill", function ( d ) {
                 var key = this.parentNode.getAttribute( "data-group" );
