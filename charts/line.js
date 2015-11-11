087d155dcb62065c472aade874823116b5f8d8a6
Roi Avinoam <avinoamr@gmail.com>
2015-11-11 21:50:51 +0200

    Rename 'stack' to 'baseline' to keep it more general

--- charts/line.js
+++ charts/line.js
@@ -1,10 +1,15 @@
 (function () {
     var color = window.color;
+    var ZERO = 0;
+    var MINIMUM = "minimum";
+    var STACK = "stack";
+    var STREAM = "stream";
+
     color.line = function () {
         var options = {
             x: null,
             y: null,
-            stack: false,
+            baseline: ZERO,
             color: null,
             palette: window.color.palettes.default,
             data: null,
@@ -16,7 +21,7 @@
         function line ( el ) { return line.draw( bar, el ) }
         line.x = getset( options, "x" );
         line.y = getset( options, "y" );
-        line.stack = getset( options, "stack" );
+        line.baseline = getset( options, "baseline" );
         line.color = getset( options, "color" );
         line.palette = getset( options, "palette" );
         line.data = getset( options, "data" );
@@ -150,10 +155,11 @@
         }
 
         // stack the data
+        var stack = that.baseline() == STACK || that.baseline() == STREAM;
         d3.layout.stack()
             .values( function ( d ) { 
                 return d.values
-            })( that.stack() ? data : [] );
+            })( stack ? data : [] );
 
         // build the scales
         var x = ( isTimeline ? d3.time.scale() : d3.scale.linear() )
@@ -247,7 +253,7 @@
             .attr( "fill", function ( d ) {
                 return c( d.key );
             })
-            .style( "opacity", that.stack() ? .4 : .1 );
+            .style( "opacity", stack ? .4 : .1 );
 
         var points = groups.selectAll( "circle[data-line-point]" )
             .data( function ( d ) { 
