fa7e3824158030eb0db88f98941d4796854d4729
Roi Avinoam <avinoamr@gmail.com>
2015-11-12 18:18:53 +0200

    Separate the interpolation logic

--- charts/line.js
+++ charts/line.js
@@ -120,7 +120,7 @@
 
         // iterate over all array-indices, until we reach an index where none of 
         // the data items have a value for - thus reaching the end.
-        var i = -1, ix;
+        var i, ix;
         for ( i = 0 ; ; i += 1 ) {
             // compute the minimal x-value that should be associated with this 
             // array-index
@@ -141,11 +141,7 @@
                 // incorrect x-coordinate for this array-index, add an 
                 // artificial point as the interpolation between the current
                 // point, and the previous one.
-                var d0 = d.values[ i - 1 ];
-                var total = d1.x - d0.x;
-                var part = ix - d0.x;
-                var y = d3.interpolate( d0.y, d1.y )( part / total );
-                d.values.splice( i, 0, { x: ix, y: y, y0: 0 } )
+                d.values.splice( i, 0, interp( d )( ix ) )
             })
         }
 
@@ -278,7 +274,7 @@
             .attr( "fill", function ( d ) {
                 var key = this.parentNode.getAttribute( "data-line-group" );
                 return c( key );
-            })
+            });
     }
 
     function xlabels ( x, y ) {
@@ -314,6 +310,20 @@
         }
     }
 
+    function interp ( d ) {
+        return function ( x ) {
+            var i = 0;
+            while ( d.values[ i ] && d.values[ i ].x < x ) { i += 1 }
+            var d1 = d.values[ i ];
+            var d0 = d.values[ i - 1 ] || d1;
+            var total = d1.x - d0.x;
+            var part = x - d0.x;
+            var dinterpolated = d3.interpolate( d0, d1 )( part / total );
+            delete dinterpolated.obj;
+            return dinterpolated;
+        }
+    }
+
     function getset ( options, key ) {
         return function ( value ) {
             if ( arguments.length == 0 ) {
@@ -324,5 +334,7 @@
             return this;
         }
     }
+
+
 
 })();
